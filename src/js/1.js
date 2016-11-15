7324ed692e8970f1e6a1e4e7049fda0ce1a44baf
chenhongdong <513948670@qq.com>
2016-11-15 19:43:38 +0800

    2016learn

---
+++ src/js/1.js
@@ -0,0 +1,6 @@
+
+
+ReactDOM.render(
+  <h1>111</h1>,
+  document.querySelector('#app')
+)
---
+++ src/js/2jsx.js
@@ -0,0 +1,13 @@
+let persons = ['优酷土豆','爱奇艺','腾讯视频','搜狐视频'];
+let style = {color : 'blue'}
+
+ReactDOM.render(
+    <div>
+      {
+        persons.map( person =>
+          <div style={style}>Hello, {person}</div>
+        )
+      }
+    </div>,
+    document.getElementById('app')
+)
