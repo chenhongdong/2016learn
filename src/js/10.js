4ec8913e64963cadca5403cb563fb7d5a7c82940
chenhongdong <513948670@qq.com>
2016-11-18 19:04:30 +0800

    DOM操作

--- src/js/10.js
+++ src/js/10.js
@@ -2,3 +2,29 @@
 * DOM操作
 *
 * */
+
+var Focus = React.createClass({
+    getInitialState(){
+        return {
+            value: 'o(>﹏<)o千万别'
+        }
+    },
+    handelClick(){
+        console.log(this.props.value)
+        console.log(this.refs.msg.value);       //o(>﹏<)o千万别
+        this.refs.msg.focus();
+    },
+    render(){
+        return (
+           <div>
+               <input type="text" ref="msg" value={this.state.value}/>
+               <button onClick={this.handelClick}>获取</button>
+           </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <Focus value="哈哈"/>,
+    document.getElementById('app')
+);
